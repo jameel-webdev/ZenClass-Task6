5d0bec6624cb431e646eda6ba79f7a007e10be1d
Jameel Ahmed <126319130+jameel-webdev@users.noreply.github.com>
2023-11-13 12:11:38 +0530

    Updated task

--- Day6-Task.js
+++ Day6-Task.js
@@ -1,90 +1,69 @@
-//Write a constructor for the class Movie, which takes a String representing the title of the movie, a String representing the studio, 
+//Write a constructor for the class Movie, which takes a String representing the title of the movie, a String representing the studio,
 //and a String representing the rating as its arguments, and sets the respective class properties to these values.
 class Movie {
+    constructor(title, studio, rating) {
+      this.title = title;
+      this.studio = studio;
+      this.rating = rating;
+    }
+  }
+  let myMovie = new Movie("Jailer", "Rajini", "8.5");
+  console.log(myMovie.title);
+  console.log(myMovie.studio);
+  console.log(myMovie.rating);
+  
+  //The constructor for the class Movie will set the class property rating to "PG" as default when no rating is provided.
+  class Movie {
     constructor(title, studio, rating = "PG") {
-        this.title = title;
-        this.studio = studio;
-        this.rating = rating;
+      this.title = title;
+      this.studio = studio;
+      this.rating = rating;
     }
-}
-var m0 = new Movie("Casino Royale", "Eon Productions", "PG13");
-//console.log(m0);
-var m1 = new Movie("Paiya", "TirupathiBros");
-var m2 = new Movie("SK", "arts");
-
-var newArray = [m0, m1, m2];
-//console.log(newArray);
-getPG = (newArray) => {
-    return newArray.filter((ele) => ele.rating == "PG");
-}
-console.log(getPG(newArray));
-
-// Class Circle
-class Circle {
-
-    constructor(radius, color) {
-        this.radius = radius;
-        this.color = color;
+  }
+  let myMovie = new Movie("Lal Salaam", "Rajini");
+  console.log(myMovie.rating);
+  //write a method get PG, which takes an array of base type Moviw as it argument, andreturns new array of only those movies in the input
+  //array with a rating of "PG".You may assume the input array is full of Movie instances. The returned array need not be full.
+  
+  //Write a piece of code that creates an instance of the class Movie with the title “Casino Royale”, the studio “Eon Productions”,
+  //and the rating “PG­13”
+  let myMovie = new Movie("Casino Royale", "Eon Productions", "PG-13");
+  console.log(myMovie);
+  
+  //Write a “person” class to hold all the details.
+  class Person {
+    constructor(name, age, address, email) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+      this.email = email;
     }
-    getArea(c1) {
-        return 3.14 * this.radius * this.radius;
+    getDetails() {
+      return `Name: ${this.name}, Age: ${this.age}, Address: ${this.address}, Email: ${this.email}`;
     }
-}
-getRadius = (c1) => {
-    return Number.isInteger(this.radius);
-}
-
-getColor = (c1) => {
-    return this.color;
-}
-getCircumference = (c1) => {
-    return 2 * 3.14 * this.radius;
-}
-var c1 = new Circle(1.0, "red");
-console.log(c1);
-//console.log(getArea(c1));
-
-// person deatials
-
-class Person {
-    constructor(Name, DOB, Address, Nationality) {
-        this.Name = Name;
-        this.DOB = DOB;
-        this.Address = Address;
-        this.Nationality = Nationality;
+  }
+  let myPerson = new Person(
+    "Jameel",
+    31,
+    "no-6,Vivekanandar street, dubai main road",
+    "jameel.webdev@gmail.com"
+  );
+  console.log(myPerson.getDetails());
+  
+  //write a class to calculate the uber price.
+  class UberPrice {
+    constructor(distance, time) {
+      this.distance = distance;
+      this.time = time;
     }
-    getDeatials() {
-        return (`the name of person ${this.Name} DOB is ${this.DOB} his Address: ${this.Address} and Nationatily ${this.Nationality}`)
+    calculatePrice() {
+      let baseFare = 2.5;
+      let perMileRate = 1.5;
+      let perMinuteRate = 0.2;
+      let distanceFare = this.distance * perMileRate;
+      let timeFare = this.time * perMinuteRate;
+      let totalFare = baseFare + distanceFare + timeFare;
+      return totalFare.toFixed(2);
     }
-}
-var person1 = new Person("AJ", "4/5/2000", "karnataka", "Indian");
-console.log(person1);
-var person2 = new Person("J", "1/1/2002", "NYC", "USA");
-console.log(person2.getDeatials())
-
-//write a class to calculate the uber price.
-
-class Uberprice {
-    constructor(distance, waitingPeriod, rate, vehicletype) {
-        this.distance = distance;
-        this.waitingPeriod = waitingPeriod;
-        this.rate = rate;
-        this.vehicletype = vehicletype;
-    }
-    setDistance(dist) {
-        this.distance = dist;
-    }
-    getRideDetails() {
-        return (`The distance of the ride is ${this.distance} with waiting period of 
-        ${this.waitingPeriod} the vehicle type is inova with AC ${this.vehicletype} and rate of ${this.rate}`)
-    }
-    getRate() {
-        let totalRate = (this.distance * this.vehicletype * this.rate) + (this.waitingPeriod * 5)
-        return totalRate;
-    }
-}
-let customer1 = new Uberprice(100, 0, 15, 5)
-console.log(customer1.getRate());
-customer1.setDistance(100);
-console.log(customer1.getRate());
-console.log(customer1.getRideDetails());
+  }
+  
